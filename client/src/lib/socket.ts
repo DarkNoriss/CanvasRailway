e4d20405641a27052845a96665438476339134d9
Patryk Przybylski <darknoriss@gmail.com>
2023-09-05 01:15:04 +0200

    feat: url to the production server

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -2,6 +2,8 @@ import type { Socket } from 'socket.io-client';
 import { io } from 'socket.io-client';
 
 const SERVER =
-  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3001';
+  process.env.NODE_ENV === 'production'
+    ? 'canvasrailway-production.up.railway.app'
+    : 'http://localhost:3001';
 
 export const socket: Socket = io(SERVER);
