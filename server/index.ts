694d00e78d32dc353d95a949069ba6e9ab16bf85
Patryk Przybylski <darknoriss@gmail.com>
2023-09-05 00:48:14 +0200

    fix: import instead of require

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
-const express = require('express')
-const http = require('http')
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+
 const app = express()
 const server = http.createServer(app)
-
-import { Server } from 'socket.io'
 
 const io = new Server(server, {
   cors: {
