8803ec6ef4477e600cf4b9c958000af5c3be6f48
Patryk Przybylski <darknoriss@gmail.com>
2023-09-05 01:47:00 +0200

    fix: url to the server

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 
 const SERVER =
   process.env.NODE_ENV === 'production'
-    ? 'canvasrailway-production.up.railway.app'
+    ? 'meowvas-server.up.railway.app'
     : 'http://localhost:3001';
 
 export const socket: Socket = io(SERVER);
