6cb18daa02ebad0ed901db19562e4581308995cb
Patryk Przybylski <darknoriss@gmail.com>
2023-09-05 05:20:30 +0200

    feat: form and validation

--- client/src/components/CreateRoomForm.tsx
+++ client/src/components/CreateRoomForm.tsx
@@ -1,6 +1,20 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import type * as z from 'zod';
+
 import { Button } from '@/components/ui/Button';
+import { createRoomSchema } from '@/lib/validations/createRoom';
 
 const CreateRoomForm = () => {
+  const form = useForm<z.infer<typeof createRoomSchema>>({
+    resolver: zodResolver(createRoomSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
+
   return (
     <form className="flex flex-col gap-4">
       <h2>Form!!!</h2>
