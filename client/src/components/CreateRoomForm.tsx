f033563f4a82ff7a2199912f527b189e7c5cef97
Patryk Przybylski <darknoriss@gmail.com>
2023-09-05 03:17:42 +0200

    feat: form to create a room

---
+++ client/src/components/CreateRoomForm.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/Button';
+
+const CreateRoomForm = () => {
+  return (
+    <form className="flex flex-col gap-4">
+      <h2>Form!!!</h2>
+      <Button>CREATE A ROOM</Button>
+    </form>
+  );
+};
+
+export default CreateRoomForm;
