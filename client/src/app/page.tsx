d6e19734d6f8d822cf000e0cdc8e9bafb5e8eba6
Patryk Przybylski <darknoriss@gmail.com>
2023-09-05 03:17:31 +0200

    feat: card with the form

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,32 @@
+import CreateRoomForm from '@/components/CreateRoomForm';
+import { Button } from '@/components/ui/Button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/Card';
+import { Separator } from '@/components/ui/Separator';
+
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <Card className="w-[90vw] max-w-sm">
+      <CardHeader>
+        <CardTitle>Meowvas</CardTitle>
+        <CardDescription>Draw with ur frens!</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col space-y-4">
+        <CreateRoomForm />
+        <div className="  flex items-center space-x-2">
+          <Separator decorative />
+          <span className="text-xs text-muted-foreground">OR</span>
+          <Separator decorative />
+        </div>
+        <Button>JOIN A ROOM</Button>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Page;
