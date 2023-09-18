fa31b16a19c5b3a2036337466288c0dbcbcc1cf1
Patryk Przybylski <darkoriss@gmail.com>
2023-09-18 04:56:36 +0200

    feat: full lobby system

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import type { IColor } from 'react-color-palette';
 
 import { Cursor } from '@/components/ui/Cursor';
 import { useDraw } from '@/hooks/useDraw';
+import type { DrawOptions } from '@/lib/drawLine';
 import { drawLine } from '@/lib/drawLine';
+import { drawWithDataURL } from '@/lib/drawWithDataURL';
 import { socket } from '@/lib/socket';
-import type { Draw, DrawLine } from '@/types/typing';
+import type { Draw } from '@/types/typing';
 
 type CanvasProps = {
   colorClient: IColor;
@@ -13,66 +16,66 @@ type CanvasProps = {
 };
 
 const Canvas = ({ colorClient, widthClient }: CanvasProps) => {
+  const [canvasLoading, setCanvasLoading] = useState(true);
+
+  const { roomId } = useParams();
+
   const createLine = ({ prevPoint, currentPoint, ctx }: Draw) => {
-    socket.emit('draw-line', {
-      prevPoint,
-      currentPoint,
-      color: colorClient,
-      width: widthClient,
-    });
-    drawLine({
-      prevPoint,
-      currentPoint,
+    const drawOptions = {
       ctx,
+      currentPoint,
+      prevPoint,
       color: colorClient,
       width: widthClient,
-    });
+    };
+
+    drawLine(drawOptions);
+
+    socket.emit('draw', { drawOptions, roomId });
   };
 
   const { canvasRef, onMouseDown, clear } = useDraw(createLine);
 
   useEffect(() => {
+    const canvasElement = canvasRef.current;
     const ctx = canvasRef.current?.getContext('2d');
 
-    socket.emit('client-ready');
+    socket.emit('client-ready', { roomId }); // works
+
+    socket.on('client-loaded', () => {
+      setCanvasLoading(false);
+    });
 
     socket.on('get-canvas-state', () => {
-      if (!canvasRef.current?.toDataURL()) return;
+      const canvasState = canvasRef.current?.toDataURL();
+      if (!canvasState) return;
 
-      socket.emit('canvas-state', canvasRef.current.toDataURL());
+      socket.emit('send-canvas-state', { canvasState, roomId });
     });
 
-    socket.on('canvas-state-from-server', (state: string) => {
-      const img = new Image();
-      img.src = state;
-      img.onload = () => {
-        ctx?.drawImage(img, 0, 0);
-      };
+    socket.on('canvas-state-from-server', (canvasState: string) => {
+      if (!ctx || !canvasElement) return;
+
+      drawWithDataURL(canvasState, ctx, canvasElement);
+      setCanvasLoading(false);
     });
 
-    socket.on(
-      'draw-line',
-      ({ prevPoint, currentPoint, color, width }: DrawLine) => {
-        if (!ctx) return;
-        drawLine({
-          prevPoint,
-          currentPoint,
-          ctx,
-          color,
-          width,
-        });
-      },
-    );
+    socket.on('update-canvas-state', (drawOptions: DrawOptions) => {
+      if (!ctx) return;
 
-    socket.on('clear', clear);
+      drawLine({ ...drawOptions, ctx });
+    });
+
+    socket.on('clear-canvas', clear);
 
     return () => {
+      socket.off('hello');
       socket.off('get-canvas-state');
       socket.off('canvas-state-from-server');
-      socket.off('draw-line');
-      socket.off('clear');
+      socket.off('update-canvas-state');
+      socket.off('clear-canvas');
     };
-  }, [canvasRef, clear]);
+  }, [canvasRef, clear, roomId]);
 
   return (
     <>
