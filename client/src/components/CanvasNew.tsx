de657e61da5029122e389fcfbb5bd29738e57fcb
DarkNoriss <104274385+DarkNoriss@users.noreply.github.com>
2023-09-20 09:53:30 +0000

    feat: buttons, onchange

--- client/src/components/CanvasNew.tsx
+++ client/src/components/CanvasNew.tsx
@@ -1,17 +1,38 @@
+import { useParams } from 'next/navigation';
 import { useRef, useState } from 'react';
+import type { CanvasPath, ReactSketchCanvasRef } from 'react-sketch-canvas';
 import { ReactSketchCanvas } from 'react-sketch-canvas';
 
+import { socket } from '@/lib/socket';
+
 const CanvasNew = () => {
+  const [paths, setPaths] = useState<CanvasPath[]>([]);
   const [strokeWidth, setStrokeWidth] = useState<number>(4);
   const [strokColor, setStrokeColor] = useState<string>('black');
 
-  const canvas = useRef(null);
+  const canvas = useRef<ReactSketchCanvasRef | null>(null);
+
+  const { roomId } = useParams();
 
   const handleClear = () => {
-    if (!canvas.current) return
-      canvas.current.clearCanvas()
-  
-    
+    canvas.current?.clearCanvas();
+    socket.emit('canvas-clear', { roomId });
+  };
+
+  const handleUndo = () => {
+    canvas.current?.undo();
+    socket.emit('canvas-undo', { roomId });
+  };
+
+  const handleRedo = () => {
+    canvas.current?.redo();
+    socket.emit('canvas-redo', { roomId });
+  };
+
+  const handleOnChange = (updatedPaths: CanvasPath[]) => {
+    console.log(updatedPaths);
+  };
+
   return (
     <>
       <ReactSketchCanvas
@@ -20,12 +41,20 @@ const CanvasNew = () => {
         height="400"
         strokeWidth={strokeWidth}
         strokeColor={strokColor}
+        onChange={handleOnChange}
       />
-      {canvas !== null ? (
-        <button onClick={handleClear} type="button">
-          CLEAR
-        </button>
-      )}
+
+      <button onClick={handleClear} type="button">
+        CLEAR
+      </button>
+
+      <button onClick={handleClear} type="button">
+        CLEAR
+      </button>
+
+      <button onClick={handleClear} type="button">
+        CLEAR
+      </button>
     </>
   );
 };
