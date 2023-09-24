4f51e20c0bd5148f1c16e541f659b7e477be740c
Patryk Przybylski <darknoriss@gmail.com>
2023-09-24 16:02:45 +0200

    cleaning

--- client/src/app/room/[roomId]/page.tsx
+++ client/src/app/room/[roomId]/page.tsx
@@ -7,7 +7,9 @@ import { useEffect } from 'react';
 
 import GameLobby from '@/components/GameLobby';
 import GameRoom from '@/components/GameRoom';
+import { socket } from '@/lib/socket';
 import { useGameStateStore } from '@/store/gameStatusStore';
+import { useMembersStore } from '@/store/membersStore';
 import { useUserStore } from '@/store/userStore';
 
 type GameStateProps = typeof gameState;
@@ -18,13 +20,26 @@ const gameState = {
 
 const Page = () => {
   const router = useRouter();
+
   const user = useUserStore((state) => state.user);
+  const setMembers = useMembersStore((state) => state.setMembers);
   const useGameState = useGameStateStore((state) => state.gameState);
+
   const currentState = gameState[useGameState as keyof GameStateProps];
 
   useEffect(() => {
     if (!user) router.replace('/');
   }, [router, user]);
+
+  useEffect(() => {
+    socket.on('update-members', ({ members }) => {
+      setMembers(members);
+    });
+
+    return () => {
+      socket.off('update-members');
+    };
+  }, [setMembers]);
 
   if (user) return currentState;
   return null;
