e00309747e3e170f6f322268f7b4ab857dbbac56
Patryk Przybylski <darknoriss@gmail.com>
2023-09-24 14:15:15 +0200

    feat: lobby before game started

---
+++ client/src/components/GameLobby.tsx
@@ -0,0 +1,49 @@
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import { Button } from '@/components/ui/Button';
+import { socket } from '@/lib/socket';
+import { useMembersStore } from '@/store/membersStore';
+
+import Countdown from './Countdown';
+
+const GameLobby = () => {
+  const [starting, setStarting] = useState(false);
+  const members = useMembersStore((state) => state.members);
+
+  const { roomId } = useParams();
+
+  useEffect(() => {
+    socket.on('start', () => setStarting(true));
+
+    return () => {
+      socket.off('start');
+    };
+  }, []);
+
+  const handleOnClick = () => {
+    socket.emit('start', { roomId });
+    setStarting(true);
+  };
+
+  return (
+    <div className="flex flex-row">
+      <div className="flex flex-col px-4">
+        <span>Members: </span>
+        <ol>
+          {members.map(({ id, username }) => (
+            <li key={id} className="ml-4 list-disc">
+              {username}
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div>
+        <Button onClick={handleOnClick}>START GAME</Button>
+      </div>
+      {starting && <Countdown />}
+    </div>
+  );
+};
+
+export default GameLobby;
