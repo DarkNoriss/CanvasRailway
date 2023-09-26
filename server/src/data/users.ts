4c220a5588abe78e66d2cd268d1ae535b90590b2
Patryk Przybylski <darknoriss@gmail.com>
2023-09-26 03:55:10 +0200

    feat: immer

--- server/src/data/users.ts
+++ server/src/data/users.ts
@@ -1,16 +1,39 @@
-import type { User } from '@/types'
+import { produce } from 'immer';
 
-const userList: User[] = []
+import type { User } from '@/types';
 
-const getUser = (userId: string) => userList.find(user => user.id === userId)
+let userList: User[] = [];
 
-const addUser = (user: User) => userList.push(user)
+const getUser = (userId: string) => userList.find((user) => user.id === userId);
 
-const removeUser = (userId: string) => userList.filter(user => user.id === userId)
+const addUser = (user: User) => {
+  userList = produce(userList, (draftState) => {
+    draftState.push(user);
+  });
+};
+
+const removeUser = (userId: string) => {
+  userList = produce(userList, (draftState) => {
+    draftState.filter((user) => user.id !== userId);
+  });
+};
 
 const getRoomMembers = (roomId: string) =>
-  userList
-    .filter(member => member.roomId === roomId)
-    .map(({ id, username }) => ({ id, username }))
+  userList.filter((member) => member.roomId === roomId);
 
-export { getUser, addUser, removeUser, getRoomMembers }
+const setUserDrawing = async (roomId: string) => {
+  userList = produce(userList, (draftState) => {
+    const membersList = getRoomMembers(roomId);
+
+    const drawing = membersList[Math.floor(Math.random() * membersList.length)];
+
+    if (!drawing) return;
+
+    const updatedUser = draftState.find((user) => user.id === drawing.id);
+    if (updatedUser) {
+      updatedUser.isDrawing = true;
+    }
+  });
+};
+
+export { addUser, getRoomMembers, getUser, removeUser, setUserDrawing };
