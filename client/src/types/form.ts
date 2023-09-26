d651b84dc2ea6ef9a7853cdb9b36fedb6f3f51a0
Patryk Przybylski <darknoriss@gmail.com>
2023-09-26 05:31:39 +0200

    feat: word schema for validation

--- client/src/types/form.ts
+++ client/src/types/form.ts
@@ -1,5 +1,8 @@
 import type { z } from 'zod';
 
 import type { createRoomSchema } from '@/lib/validations/roomForm';
+import type { roomWordSchema } from '@/lib/validations/roomWord';
 
 export type RoomType = z.infer<typeof createRoomSchema>;
+
+export type RoomWordType = z.infer<typeof roomWordSchema>;
