26275196083267182ca703a8266b72bc9941a7a9
Patryk Przybylski <darknoriss@gmail.com>
2023-09-26 03:57:52 +0200

    feat: game state where user is picking the word

--- client/src/components/GamePicking.tsx
+++ client/src/components/GamePicking.tsx
@@ -1,7 +1,20 @@
+import { Button } from '@/components/ui/Button';
+import { socket } from '@/lib/socket';
+import { useUserStore } from '@/store/userStore';
+
 const GamePicking = () => {
+  const user = useUserStore((state) => state.user);
+
   return (
     <div className="flex justify-center">
-      <span>Host is picking a word...</span>
+      {user?.isDrawing ? (
+        <div>
+          <span>Enter a word!</span>
+          <Button onClick={() => socket.emit('check')}>CHECK ARRAY</Button>
+        </div>
+      ) : (
+        <span>Host is picking a word...</span>
+      )}
     </div>
   );
 };
